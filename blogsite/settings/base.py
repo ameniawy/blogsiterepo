754be8effb48c77e53510a5da964f1c313dbf9b0
ameniawy <ameniawy18@gmail.com>
2016-06-23 16:48:20 +0200

    final touches

--- blogsite/settings/base.py
+++ blogsite/settings/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'blogsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/abdelrahman/Desktop/blogsite/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
