ca80a968ea14e41c72e4215965c0e87a92cc7bd8
ameniawy <ameniawy18@gmail.com>
2016-06-24 02:26:30 +0200

    lets try this now after changing production settings

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -8,7 +8,7 @@ DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -27,3 +27,9 @@ SHARE_URL = "http://launchwithcode.com/?ref="
  STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
  )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+    }
+}
